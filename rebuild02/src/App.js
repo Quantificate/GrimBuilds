2c0449c46a4a23e635bc358f410f5b5471219e56
Quantificate <thatguyfromct@gmail.com>
2019-04-28 14:55:33 -0700

    Getting Started

--- rebuild02/src/App.js
+++ rebuild02/src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
+import {DropdownButton, Dropdown} from '/node_modules/react-bootstrap';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -18,8 +17,12 @@ function App() {
         >
           Learn React
         </a>
+        <DropdownButton id="classButton" title="Class Dropdown">
+            <Dropdown.Item href="#">Archon</Dropdown.Item>
+        </DropdownButton>
       </header>
     </div>
+
   );
 }
 
